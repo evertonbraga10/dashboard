7beff3b3860a34e0d8b762ac6411e1a1f5d10ded
felipegael <gael.ferreira505@gmail.com>
2023-04-05 21:31:00 -0300

    atualização dashboard

--- dashboard/js/scripts.js
+++ dashboard/js/scripts.js
@@ -1,4 +1,8 @@
 let troca = document.querySelector(".troca");
+let body = document.querySelector("body");
+let header = document.querySelector("header");
 troca.addEventListener("click", ()=>{
     troca.classList.toggle("active");
-})
+    body.classList.toggle("dark");
+    header.classList.toggle("active");
+});
